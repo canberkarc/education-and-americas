7cbafd27aac63bfa0f4199209a894b0e8b451e8d
canberkarc <canberkaricii@gmail.com>
2024-12-10 22:00:20 +0100

    add existence check for .db file in tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -1,1 +1,11 @@
 python -m pytest tests.py
+
+
+# Check if the .db file is created
+DB_FILE_PATH="/home/runner/work/education-and-americas/education-and-americas/project/data/educationAndEconomy_BrazilColombiaPeru.db"
+if [ -f "$DB_FILE_PATH" ]; then
+    echo "$DB_FILE_PATH exists."
+else
+    echo "$DB_FILE_PATH does not exist."
+    exit 1
+fi
