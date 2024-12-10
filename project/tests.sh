a9ebb33371a337e807098a3de1669b839b46cfde
canberkarc <canberkaricii@gmail.com>
2024-12-10 21:56:34 +0100

    update CI workflow to use absolute path for database artifact

--- project/tests.sh
+++ project/tests.sh
@@ -1,11 +1,1 @@
 python -m pytest tests.py
-
-
-# Check if the .db file is created
-DB_FILE_PATH="project/data/educationAndEconomy_BrazilColombiaPeru.db"
-if [ -f "$DB_FILE_PATH" ]; then
-    echo "$DB_FILE_PATH exists."
-else
-    echo "$DB_FILE_PATH does not exist."
-    exit 1
-fi
