adc8a1bf3bd0b3e7aff9e2619fe10a91604f8e29
canberkarc <canberkaricii@gmail.com>
2024-12-10 21:19:24 +0100

    update extract.py to run Firefox in headless mode and adjust .gitignore for .db files

--- project/ETL/extract.py
+++ project/ETL/extract.py
@@ -60,7 +60,7 @@ def extractGdpData():
     try:
         # Extract GDP data
         logging.info("Starting Firefox...")
-        driver = helium.start_firefox()
+        driver = helium.start_firefox(headless=True)
 
         sleep(1)
         logging.info("Downloading GDP data...")
@@ -91,7 +91,7 @@ def extractIlliteracyRateData():
         try:
             # Extract illiteracy rate data
             logging.info("Starting Firefox...")
-            driver = helium.start_firefox()
+            driver = helium.start_firefox(headless=True)
     
             logging.info("Opening illiteracy rate data website...")
             url = "https://statistics.cepal.org/portal/databank/index.html?lang=en&indicator_id=53&area_id=408&members=43053%2C146%2C327%2C2172%2C29170%2C29171%2C29172%2C29173%2C29174%2C29175%2C29176%2C29177%2C29178%2C29179%2C29180%2C29181%2C29182%2C29183%2C29184%2C29185%2C29186%2C29187%2C29188%2C29189%2C29190%2C29191%2C29192"
@@ -125,7 +125,7 @@ def extractYearsOfEducationData():
 
     try:
         logging.info("Starting Firefox...")
-        driver = helium.start_firefox()
+        driver = helium.start_firefox(headless=True)
 
         logging.info("Opening years of education data website...")
         url = 'https://statistics.cepal.org/portal/databank/index.html?lang=en&indicator_id=1781&area_id=408&members=43053%2C146%2C327%2C1431%2C1432%2C1433%2C1434%2C29170%2C29171%2C29172%2C29173%2C29174%2C29175%2C29176%2C29177%2C29178%2C29179%2C29180%2C29181%2C29182%2C29183%2C29184%2C29185%2C29186%2C29187%2C29188%2C29189%2C29190%2C29191%2C29192'
@@ -165,7 +165,7 @@ def extractEconomicActivityParticipationData():
     
     try:
         logging.info("Starting Firefox...")
-        driver = helium.start_firefox()
+        driver = helium.start_firefox(headless=True)
 
         logging.info("Opening economic activity participation data website...")
         url = 'https://statistics.cepal.org/portal/databank/index.html?lang=en&indicator_id=1788&area_id=408&members=43053%2C146%2C327%2C1431%2C1432%2C1433%2C1434%2C29170%2C29171%2C29172%2C29173%2C29174%2C29175%2C29176%2C29177%2C29178%2C29179%2C29180%2C29181%2C29182%2C29183%2C29184%2C29185%2C29186%2C29187%2C29188%2C29189%2C29190%2C29191%2C29192'
@@ -206,7 +206,7 @@ def extractEconomicActivityParticipationData():
 def extractPublicExpenditureData():
     try:
         logging.info("Starting Firefox...")
-        driver = helium.start_firefox()
+        driver = helium.start_firefox(headless=True)
 
         logging.info("Opening public expenditure data website...")
         url = 'https://statistics.cepal.org/portal/databank/index.html?lang=en&indicator_id=460&area_id=408&members=216%2C1409%2C1411%2C29138%2C29139%2C29140%2C29141%2C29142%2C29143%2C29144%2C29145%2C29146%2C29147%2C29150%2C29151%2C29152%2C29153%2C29154%2C29155%2C29156%2C29157%2C29158%2C29159%2C29160%2C29161%2C29162%2C29163%2C29164%2C29165%2C29166%2C29167%2C29168%2C29169%2C29170%2C29171%2C29172%2C29173%2C29174%2C29175%2C29176%2C29177%2C29178%2C29179%2C29180%2C29181%2C29182%2C29183%2C29184%2C29185%2C29186%2C29187%2C29188%2C29189%2C29190%2C29191%2C29192%2C29193'
